68a8dedc4728492d468075335d80a8050b170da1
coderun13 <snehalsingh.0407@gmail.com>
2024-07-08 11:21:23 +0530

    hello world program

---
+++ helloworld.py
@@ -0,0 +1,44 @@
+msg = "hello world"
+
+print(msg.split())
+
+# print(msg.capitalize())
+
+print(msg)
+print("Hello Snehal")
+print("How are you?")
+print("Hello Snehal", "nice to see you here.")
+print(19)
+print(12+18)
+
+#Charcter Set
+# A to Z, a to z
+# Digit- 0-9
+# Special Character= + - * /
+# Whitespace= Blank space,tab,carriage return,newline,formfeed
+# Other Characters - Python can process all ASCII and Unicode characters as part of data or literals
+
+
+#Input two numbers and print sum
+
+first = int(input("Enter first number: "))
+second  = int(input("Enter second number: "))
+
+print("sum =", first + second)
+
+#Input side of a square and print its area
+
+Side = int(input("Enter side of a square:"))
+print("area =", Side*Side)
+
+#Input 2 floating point numbers and print average
+
+a= float(input("First number:"))
+b= float(input("Second number:"))
+print("avg is:",(a+b)/2)
+
+#input 2 numbers and print True if a is greater than or equal to b. If not print False 
+
+num1 = int(input("Enter num1: "))
+num2 = int(input("enter num2: "))
+print(num1 >= num2)
