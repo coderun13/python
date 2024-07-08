53ca4afdcd60054ff2d9a40120ff075d6aac1f6f
coderun13 <snehalsingh.0407@gmail.com>
2024-07-08 13:35:20 +0530

    practice question

---
+++ Practice Question/string.py
@@ -0,0 +1,10 @@
+#take user name as input and print its length
+
+name= input("What's your name: ")
+print(len(name))
+
+#find the occurence of $ in a string
+
+str2 = "I am $ symbol here"
+print(str2.find("$"))
+print(str2.count("$"))
