cad15576203af6a7b29cfc8de75072c25c1f1622
coderun13 <snehalsingh.0407@gmail.com>
2024-08-02 14:57:01 +0530

    modified

--- Practice Question/multiple.py
+++
@@ -1,8 +0,0 @@
-# check if number is a multiple of 7 or not
-
-num =int(input("Enter a number: "))
-
-if(num%7 == 0):
-    print("It is a multiple of 7")
-elif(num%7 != 0):
-    print("It is not a multiple of 7")
--- Practice Question/operators.py
+++
@@ -1,9 +0,0 @@
-a = 30
-b = 6
-sum = a + b
-print(sum)
-print(a-b)
-print(a*b)
-print(a/b) #quotient
-print(a%b) #remainder
-print(a**b) #a^b
