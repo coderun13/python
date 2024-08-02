01b01489378f2a48031b844a728eca941c486761
coderun13 <snehalsingh.0407@gmail.com>
2024-08-02 14:19:39 +0530

    add new file

--- Topics/list.py
+++ Topics/list.py
@@ -1,9 +1,19 @@
 # A built-in data type that stores set of values
-# It can store elements of different types
+# It can store elements of different types(int,float,string)
+# string -> immutable(can access but can't change)
+# lists -> mutable (can change)
 
+marks = [45,34,56,78,91]
+print(marks)
+print(type(marks))
+print(len(marks))
+print(marks[0])
+print(marks[1])
 
+student = ["aryan", 91, 13, "bokaro"]
+print(student)
 
-
+#list slicing
 
 
 
