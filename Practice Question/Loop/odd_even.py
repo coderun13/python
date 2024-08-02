1f142f7e184a77b158511482c2da25bd84fb4b74
coderun13 <snehalsingh.0407@gmail.com>
2024-08-02 14:18:01 +0530

    add new file

---
+++ Practice Question/Loop/odd_even.py
@@ -0,0 +1,9 @@
+num = int(input("Enter a number: "))
+if num%2 ==0 and num != 0:
+    print("Even number")
+elif num%2 != 0:
+    print("Odd number")
+elif num == 0:
+    print("The number is 0")
+else:
+    print("the number is invalid")
---
+++ Practice Question/Loop/positive_neg_num.py
@@ -0,0 +1,8 @@
+num = int(input("enter a number: "))
+if num > 0 :
+    print("It is a postive number")
+elif num == 0:
+    print("The number is 0")
+else :
+    print("It is a negative number")    
+
