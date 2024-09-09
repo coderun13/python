8825df5115906278d09bbe05a97dfb4604ac5db6
coderun13 <snehalsingh.0407@gmail.com>
2024-09-09 14:14:36 +0530

    lists

--- Topics/list.py
+++ Topics/list.py
@@ -14,6 +14,39 @@ student = ["aryan", 91, 13, "bokaro"]
 print(student)
 
 #list slicing
+# list_name[starting_idx: ending_idx](ending idx is not included)
 
+print(marks[0:3])
+print(marks[1:])
+print(marks[-3:-1])
 
+# List Methods
+# list.append() -> adds one element at the end
+# list.sort() -> sorts in ascending order 
+# list.sort(reverse=True) -> sorts in descending order 
+# list.reverse() -> reverse list 
+# list.insert(idx,el) -> insert element at index
+# list.remove -> removes first occurence of element
+# list.pop(idx) ->removes element at idx
 
+list = [2,1,3]
+list.append(4)
+print(list)
+
+list.sort()
+print(list)
+
+list.sort(reverse=True)
+print(list)
+
+list.reverse()
+print(list)
+
+list.insert(1, 5)
+print(list)
+
+list.remove(5)
+print(list)
+
+list.pop(2)
+print(list)
