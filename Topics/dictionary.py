10e427f5ffe250e8beaa0f00a067ae8695f132c1
coderun13 <snehalsingh.0407@gmail.com>
2024-09-20 10:50:45 +0530

    add new file

---
+++ Topics/dictionary.py
@@ -0,0 +1,11 @@
+# They are used to store data values in key:value pairs
+# They are unordered, mutable(changeable) and don't allow duplicate keys
+
+info = {
+    "name": "snehal",
+    "cgpa": 8.1,
+    "marks": [98,97,95],
+}
+
+print(info)
+print(type(info))
