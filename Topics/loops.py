c02b172ca4ee8b6908109c612415fe074e21908a
coderun13 <snehalsingh.0407@gmail.com>
2024-10-08 00:26:53 +0530

    modified

--- Topics/loops.py
+++ Topics/loops.py
@@ -1,7 +1,7 @@
 # Loops are used to repeat instructions
 # While loop
-# 
-# 
+# Break: Used to terminate the loop when conected
+# Continue: Terminates execution in the current iteration and continues execution of the loop with next iteration
 # 
 
 count = 1
