1a8cfd950856fc573cffba1df05cc7ef041d56af
coderun13 <snehalsingh.0407@gmail.com>
2024-12-07 18:35:18 +0530

    add new files

---
+++ Practice Question/List/elements_in_list.py
@@ -0,0 +1,8 @@
+hero = ["ironman","thor","batman","caption america"]
+
+def print_list(hero):
+    print(list(hero))
+
+print(hero[0],end=" ") #"\n"->print in next line
+print(hero[2])
+print_list(hero)
---
+++ Practice Question/List/length_of_list.py
@@ -0,0 +1,6 @@
+list = ["delhi","bokaro","MP","UP","Bihar","Chennai"]\
+
+def print_len(list):
+    print(len(list))
+
+print_len(list)
