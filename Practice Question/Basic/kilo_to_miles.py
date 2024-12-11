a42072418e668636ef03f31cd75cc9aed57fdee6
coderun13 <snehalsingh.0407@gmail.com>
2024-12-12 00:16:41 +0530

    file modify

--- Practice Question/Basic/kilo_to_miles.py
+++ Practice Question/Basic/kilo_to_miles.py
@@ -1,4 +1,5 @@
+#Logic = take input as km then formula is km * 0.62 for miles then print
+
 km = int(input("enter the distance in kilometer: "))
 miles = km*0.621371
 print("the value in miles is: ",miles)
-
