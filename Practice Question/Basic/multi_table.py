bfe9d66c182b4190642c8fe7da0b3f19a1679bd7
coderun13 <snehalsingh.0407@gmail.com>
2024-12-13 00:08:19 +0530

    file modify

---
+++ Practice Question/Basic/multi_table.py
@@ -0,0 +1,9 @@
+# n = int(input("Enter a number: "))
+# for i in range(1,11):
+#     print(n ,"*", i ,"=", n*i)
+
+i = 0
+m = int(input("enter a number: "))
+while (i <=10 ):
+    print(m, "*", i, "=", m*i)
+    i= i+1
