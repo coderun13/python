b594454e55d33f82d7f732f9710e7f394c11d891
coderun13 <snehalsingh.0407@gmail.com>
2024-12-15 01:57:18 +0530

    enumerate function

---
+++ Topics/enumerate_fucntion.py
@@ -0,0 +1,10 @@
+"""
+This function mainly assign the index 
+"""
+
+L = ["Snehal", "Aryan", "Goli", "Icey"]
+for index,name in enumerate(L):
+    print(index,name)
+
+if index == 3:
+    print(name)
