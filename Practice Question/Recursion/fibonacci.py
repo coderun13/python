a7dd3fd35711d29243d965d4de57aa744b280233
coderun13 <snehalsingh.0407@gmail.com>
2025-01-21 16:02:36 +0530

    modify file

--- Practice Question/Recursion/fibonacci.py
+++ Practice Question/Recursion/fibonacci.py
@@ -1,6 +1,7 @@
 # logic = 0, 1, 1, 2, 3, 5, 8, 13, ....
 # a = 0 , b = 1 , c = (a+b), then a = b, b = c, c = a+b
-# take m as input then if m  1 then print a else print a and b for c range is (2 to m+1) c= a+b, a = b, b = c then print c 
+# take m as input then if m  1 then print a else print a and b for c range 
+# is (2 to m+1) c= a+b, a = b, b = c then print c 
 a = 0
 b = 1
 
